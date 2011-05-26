a859fb9d695a89d62ff62b89abe7001bea2bf835
carsanlit <carsanlit@gmail.com>
2011-05-26 04:11:32 +0000

    Primer Commit.

---
+++ rs-gestion_stock/src/main/java/pe/edu/upc/dsd/rs/StockService.java
@@ -0,0 +1,78 @@
+package pe.edu.upc.dsd.rs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+@Path("/Stock")
+public class StockService {
+
+	@GET
+	@Path("/asignar/{codigorecurso}/{codigopedido}")
+	@Produces("text/plain")
+	public String asignarRecurso(@PathParam("codigorecurso") String codigorecurso, @PathParam("codigopedido") String codigopedido) {
+		return "Asignado";
+	}
+	
+	@GET
+	@Path("/consultar/{codigorecurso}/{fecha}/{hora}")
+	@Produces("text/plain")
+	public String consultarRecurso(@PathParam("codigorecurso") String codigorecurso, @PathParam("fecha") String fecha, @PathParam("hora") String hora) {
+		String msj="";
+		if (codigorecurso.equals("RD001")) {
+			msj="Disponible";
+		} else { 
+			if (codigorecurso.equals("RD002")) {
+				msj="Disponible";
+			} else {
+				if (codigorecurso.equals("RD003")) {
+					msj="Disponible"; 
+				} else {
+					if (codigorecurso.equals("RD004")) {
+						msj="No Disponible"; 
+					} else {
+						if (codigorecurso.equals("RM001")) {
+							msj="No Disponible"; 
+						} else {
+							if (codigorecurso.equals("RM002")) {
+								msj="Disponible"; 
+							} else {
+								msj="Recurso " + codigorecurso + " no Encontrado";
+							}
+						}
+					}
+				}
+			}
+		}
+		return msj;
+	}
+
+	@GET
+	@Path("/listar/{tiporecurso}")
+	@Produces("text/plain")
+	public String listarRecursos(@PathParam("tiporecurso") int tipoRecurso) {
+		String lista="";
+		switch (tipoRecurso)
+		{
+		case 0: 
+			//Recurso - Despachador
+			lista="RD001,RD002,RD003,RD004";
+			break;
+		case 1:
+			//Recurso - Movilidad
+			lista="RM001,RM002";
+			break;			
+		default:
+			//Recurso - Libre
+			lista="RL001";
+			break;
+		}
+		return lista;
+	}
+
+
+}
